864c842e9dd9e0c53aa5fe12e55ebb24e576ae54
Jann Deterling <jann.deterling@thoughtworks.com>
2020-01-26 14:03:49 +0100

    [Jann] adds multiple controller methods

--- src/SampleController.ts
+++ src/SampleController.ts
@@ -1,16 +1,20 @@
-import { Controller, Get } from "@rokkit.ts/web";
+import { Controller, Delete, Get, Post, Put } from "@rokkit.ts/web";
 import { Inject } from "@rokkit.ts/core";
-import { RequestParam } from "@rokkit.ts/web/lib/component/httpRequestParameterDecorators";
+import {
+  RequestBody,
+  RequestParam
+} from "@rokkit.ts/web/lib/component/httpRequestParameterDecorators";
 
 @Controller("/sample")
 export class SampleController {
-  private readonly greet: string;
+  private greet: string;
 
   constructor(@Inject("Hello") greet: string) {
     this.greet = greet;
   }
 
   @Get("")
+  @Get("/")
   greetWorld(): string {
     return `${this.greet} World`;
   }
@@ -18,5 +22,23 @@ export class SampleController {
   @Get("/:name")
   greetName(@RequestParam("name") name: string): string {
     return `${this.greet} ${name}`;
+  }
+
+  @Post("")
+  @Post("/")
+  changeGreetPhrase(@RequestBody() body: { greet: string }) {
+    console.log(body);
+    this.greet = body.greet;
+  }
+
+  @Delete("")
+  @Delete("/")
+  restoreDefaultGreetPhrase() {
+    this.greet = "Hello";
+  }
+
+  @Put("/:name")
+  changeName(@RequestBody() body: { name: string }) {
+    return `${this.greet} ${body.name}`;
   }
 }
